12ffc7e593c3ded2520140ada04951cd463834eb
lkkhanh2000 <1851050060khanh@ou.edu.vn>
2024-03-05 21:10:09 +0700

    test

---
+++ sever.js
@@ -0,0 +1,21 @@
+const jsonServer = require('json-server');
+const path = require('path');
+const express = require('express');
+
+const server = express();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use('/api', router);
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.use(middlewares);
+
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
